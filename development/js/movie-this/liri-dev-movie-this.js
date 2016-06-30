a8a5cf728807dd5b2279414ce721f581dfc15962
RamblinRoyce <ChingRoyce@gmail.com>
2016-06-30 18:27:27 -0500

    rching movie-this parse response complete, begin bad response/no movie found

--- development/js/movie-this/liri-dev-movie-this.js
+++ development/js/movie-this/liri-dev-movie-this.js
@@ -271,11 +271,18 @@ if (cmdinstr == "my-tweets") {
 				// * Plot
 				// * Actors
 				// * Rotten Tomatoes Rating 
-				// * Rotton Tomatoes UrL
+				// * Rotten Tomatoes UrL
 
-			console.log(response)
+			// console.log(response)
 			console.log("Title: " + JSON.parse(body)["Title"])
 			console.log("Release Year: " + JSON.parse(body)["Year"])
+			console.log("IMDB Rating: " + JSON.parse(body)["imdbRating"])
+			console.log("Country: " + JSON.parse(body)["Country"])
+			console.log("Language: " + JSON.parse(body)["Language"])
+			console.log("Plot: " + JSON.parse(body)["Plot"])
+			console.log("Actors: " + JSON.parse(body)["Actors"])
+			console.log("Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"])
+			console.log("Rotten Tomatoes URL: " + JSON.parse(body)["tomatoURL"])
 		}
 	});
 
