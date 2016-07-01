d05ba78928dd8c12e428473b64eb0621b9eab697
RamblinRoyce <ChingRoyce@gmail.com>
2016-07-01 18:17:02 -0500

    final commit

--- js/keys.js
+++
@@ -1,8 +0,0 @@
-console.log('twitterKeys is loaded');
-
-exports.twitterKeysSource = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
-}
